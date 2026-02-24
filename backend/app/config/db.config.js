21be12e9b391f9f82939727456096c7548ddcd6d
Nithya Bharathi <nithyabharathi77@gmail.com>
2026-02-24 15:09:46 +0530

    Update connection strings for online API and MongoDB

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: process.env.MONGO_URL || "mongodb://localhost:27017/dd_db"
+  url: process.env.MONGO_URL || "mongodb+srv://db_user:db_password@cluster0.example.mongodb.net/dd_db?retryWrites=true&w=majority" // Replace with actual online MongoDB URI
 };
