c409f793a4376a225dbfcb7375db8f709f923739
Nithya Bharathi <nithyabharathi77@gmail.com>
2026-02-24 16:19:51 +0530

    Update db.config.js with real MongoDB Atlas URI

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: process.env.MONGO_URL || "mongodb+srv://db_user:db_password@cluster0.example.mongodb.net/dd_db?retryWrites=true&w=majority" // Replace with actual online MongoDB URI
+  url: process.env.MONGO_URL || "mongodb+srv://nithiyab:Nithiya%4081247@cluster0.3l27zop.mongodb.net/dd_db?appName=Cluster0"
 };
